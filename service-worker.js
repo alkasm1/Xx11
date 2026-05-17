46916a4171e4bb1c34f7c03f7b8c46db429bdae1
رأفت عمر <shamwebs1@gmail.com>
2026-05-17 18:30:51 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+// FILE: /service-worker.js
+
+const C="alm-cache";
+const A=[
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./alm/core.js",
+  "./app/ui.js",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(C).then(c=>c.addAll(A)));
+});
+
+self.addEventListener("activate",e=>{
+  e.waitUntil(
+    caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))
+  );
+});
+
+self.addEventListener("fetch",e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  );
+});
